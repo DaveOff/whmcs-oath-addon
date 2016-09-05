be704c6907938fa33059faa0b44ab43fca0e4666
DaveOff <golchinha.org@gmail.com>
2016-09-05 17:39:19 +0430

    add persian language

---
+++ modules/addons/oath/lang/persian.php
@@ -0,0 +1,23 @@
+<?php
+$_ADDONLANG['incorrect']='کد وارد شده اشتباه است.';
+$_ADDONLANG['enterCodeNote']='لطفا کد ساخته شده توسط موبایل را وارد نمایید';
+$_ADDONLANG['enterCode']='کد را وارد کنید';
+$_ADDONLANG['btnLogin']='اعتبارسنجی ورود';
+$_ADDONLANG['enterEmCode']='کد اضطراری را وارد کنید
+$_ADDONLANG['emLogin']='ورود اضطراری';
+$_ADDONLANG['lostDevice']='موبایل خود را گم کرده اید؟';
+$_ADDONLANG['inactive']='پشتیبانی تایید هویت دو عاملی در حال حاظر غیرفعال است';
+$_ADDONLANG['disabled']='تایید هویت دو عاملی در حال حاظر فعال است.';
+$_ADDONLANG['btnEnable']='فعال سازی تایید هویت دو عاملی';
+$_ADDONLANG['scanNote']='لطفا توسط تلفن همراه خود QR کد زیر را اسکن کنید';
+$_ADDONLANG['unableScan']='اگر قادر به اسکن  کد QR نیستید از کد زیر استفاده کنید :';
+$_ADDONLANG['verify']='کد فعال سازی';
+$_ADDONLANG['recomApp']='برنامه های پیشنهاد شده';
+$_ADDONLANG['enabled']='تایید هویت دو عاملی در حال حاظر فعال است';
+$_ADDONLANG['emCode']='کد اضظراری شما :';
+$_ADDONLANG['emCodeNote']='در صورت گم شدن تلفن همراه خود میتوانید از کد زیر استفاده کنید :';
+$_ADDONLANG['btnSecret']='دیدن کد;
+$_ADDONLANG['emCodeNote2']='این کد را در جایی یادداشت و نگهداری کنید به دلیل اینکه دیگر نمایش داده نخواهد شد';
+$_ADDONLANG['btnDisable']='غیرفعال سازی تاید هویت دو عاملی';
+$_ADDONLANG['note']='توجه: دسترسی به اکانت  نیازمند کد تایید هویت دو عاملی میباشد';
+?>
